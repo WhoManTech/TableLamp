840a2469b2aae7c26f1c0e4a1545854f5b9747b0
Enes <enes515254@gmail.com>
2019-09-30 01:11:30 +0600

    Fixed some bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,81 @@
 #include <Arduino.h>
+#include <OneButton.h>
 
-#define LAMP_SETT_PIN 1
-#define PIR_PIN 3
+#define LAMP_SETT_PIN A1
 #define LAMP_PIN 0
+#define PIR_PIN 3
 #define FAN_PIN 4
-#define FAN_SETT_PIN 5
+#define BUTT_PIN 1
+#define TIMER 10000
+
+OneButton button(BUTT_PIN, false);
+
+unsigned long timer;
 
 int bright;
 int fan_speed;
+int move_inf;
+
+bool light_flag = true;
+bool fan_up_flag = true;
 
 
 
-void setup() {
+void click1(){
+  light_flag = !light_flag;
+  analogWrite(FAN_PIN, 255);
+}
+
+void longPress(){
+  if(fan_up_flag){
+    fan_speed++;
+  }
+  else if(!fan_up_flag){
+    fan_speed--;
+  }
+}
+
+void longPressStop(){
+  fan_up_flag = !fan_up_flag;
+}
+
+
+void setup(){
   pinMode(LAMP_SETT_PIN, INPUT);
-  pinMode(PIR_PIN, INPUT);
   pinMode(LAMP_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
-  pinMode(FAN_SETT_PIN, INPUT);
+  //pinMode(BUTT_PIN, INPUT_PULLUP);
+
+  button.attachClick(click1);
+  //button.attachDuringLongPress(longPress);
+  //button.attachLongPressStop(longPressStop);
 }
 
 
 
 void loop(){
-  bright = map(analogRead(LAMP_SETT_PIN), 0, 1023, 0, 255);
-  if(digitalRead(PIR_PIN) == 1){
-    digitalWrite(FAN_PIN, HIGH);
+  button.tick();
+  bright = map(analogRead(LAMP_SETT_PIN), 0, 1032, 0, 255);
+
+
+  if(light_flag){
+    analogWrite(LAMP_PIN, bright);
   }
-  else{
-    digitalWrite(FAN_PIN, LOW);
+  else if(!light_flag){
+    analogWrite(LAMP_PIN, 0);
+  }  
+
+  if(move_inf == 1){
+    if(light_flag){
+       if(millis() - timer > TIMER){
+        timer = millis();
+        light_flag = false;
+      }
+    }
+    else{
+      light_flag = true;
+    }
   }
-  analogWrite(LAMP_PIN, bright);
+  
+
 }
