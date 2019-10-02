a092ccd4f6c0cccecf3fc2fd0cca5ed43bb2e140
Enes <enes515254@gmail.com>
2019-10-02 23:30:02 +0600

    Fixed button and added effect

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 #define PIR_PIN 3
 #define FAN_PIN 4
 #define BUTT_PIN 1
-#define TIMER 10000
+#define TIMER 600000
 #define TIMER_SEARCH 5000
+#define TIMER_EFFECT 10
 
 OneButton button(BUTT_PIN, false);
 
@@ -21,19 +22,35 @@ int move_inf;
 bool light_flag = true;
 bool fan_up_flag = true;
 bool fan_flag = false;
-bool search_flag;
+bool effect_flag;
 
 
 
 void click1(){
   light_flag = !light_flag;
-  search_flag = false;
-  delay(2000);
 }
 
 void doubleClick(){
   fan_flag = !fan_flag;
 }
+
+
+
+void switchEffectOn(){
+  for(int i = 0; i <= bright; i++){
+    analogWrite(LAMP_PIN, i);
+    delay(10);
+  }
+}
+
+void switchEffectOff(){
+  for(int i = bright; i >= 0 ; i--){
+    analogWrite(LAMP_PIN, i);
+    delay(10);
+  }
+}
+
+
 
 
 void setup(){
@@ -49,16 +66,20 @@ void setup(){
 
 void loop(){
   button.tick();
-  bright = map(analogRead(LAMP_SETT_PIN), 0, 1023, 0, 255);
+  bright = map(analogRead(LAMP_SETT_PIN), 0, 1032, 0, 255);
   move_inf = digitalRead(PIR_PIN);
 
 
   if(light_flag){
+    if(!effect_flag){
+      effect_flag = true;
+      switchEffectOn();
+    }
+
     analogWrite(LAMP_PIN, bright);
-  //  if(bright >= 200){
-    //  analogWrite(FAN_PIN, 255);
-    //  fan_flag = true;
-   // }
+    if(bright >= 200){
+      fan_flag = true;
+   }
 
     if(fan_flag){
       analogWrite(FAN_PIN, 255);
@@ -77,6 +98,10 @@ void loop(){
   }
 
   else if(!light_flag){
+    if(effect_flag){
+      effect_flag = false;
+      switchEffectOff();
+    }
     analogWrite(LAMP_PIN, 0);
     analogWrite(FAN_PIN, 0);
     if(move_inf == 1){
