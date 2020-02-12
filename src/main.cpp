ce51d883014330ee50ea58eba94a1fc912ca4f5e
Enes <enes515254@gmail.com>
2020-02-12 18:03:23 +0600

    Fix click switch

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ bool search_flag;
 void click1(){
   if (lamp.isOn()) {
     lamp.off();
+    fan.off();
   }
   else {
     lamp.on();
@@ -55,31 +56,7 @@ void loop(){
   lamp.update();
   fan.update(lamp.getBrightness());
 
-  // bright = map(analogRead(LAMP_SETT_PIN), 0, 1032, 0, 255);
-  // move_inf = digitalRead(PIR_PIN);
 
-
-  // if(light_flag){
-  //   if(!effect_flag){
-  //     effect_flag = true;
-  //     switchEffectOn();
-  //   }
-
-  //   analogWrite(LAMP_PIN, bright);
-  //   if(bright >= 200){
-  //     fan_flag = true;
-  //   }
-
-  //   if(fan_flag){
-  //     analogWrite(FAN_PIN, 255);
-  //   }
-  //   else if(!fan_flag){
-  //     analogWrite(FAN_PIN, 0);
-  //   }
-
-  //   if(move_inf == 1){
-  //     timer = millis();
-  //   }
   if (lamp.isOn()) {
     if(millis() - timer > 600000){
       timer = millis();
@@ -88,26 +65,4 @@ void loop(){
     }
   }
 
-
-  // else if(!light_flag){
-  //   if(effect_flag){
-  //     effect_flag = false;
-  //     switchEffectOff();
-  //   }
-  //   analogWrite(LAMP_PIN, 0);
-  //   analogWrite(FAN_PIN, 0);
-
-  //   if(!search_flag){
-  //     if(millis() - search_timer > TIMER_SEARCH){
-  //       search_timer = millis();
-  //       search_flag = true;
-  //     }
-  //   }
-  //   else if(search_flag){
-  //     if(move_inf == 1){
-  //       light_flag = true;
-  //       search_flag = false;
-  //     }
-  //   }
-  // }
 }
